8689625209fd58503ea80f2ffb98da36a77c2da0
Danilo Torchio <danilotorchio@gmail.com>
2025-10-12 18:41:17 -0300

    Add prettier config and chadcn

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,38 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  devIndicators: {
+    position: 'bottom-right',
+  },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'strict-origin-when-cross-origin',
+          },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=31536000; includeSubDomains',
+          },
+          // {
+          //   key: 'Content-Security-Policy',
+          //   value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline';",
+          // },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
